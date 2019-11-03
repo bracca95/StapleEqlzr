ba06fd926fade871ec8534443c56b2e1156e9ad0
Lorenzo Braccaioli <lbraccaioli.falcone@gmail.com>
2019-11-03 14:42:18 +0100

    good class Player

--- Player.py
+++ Player.py
@@ -3,23 +3,16 @@ from pydub.playback import play
 import io
 
 class Player:
-    # private variables
-    __rawData = None
-    __song = None
-    __path = None
-    __formato = None
 
     # parametrized constructor
     def __init__(self, path, formato):
-        self.__rawData = None
-        self.__song = None
         self.__path = path
         self.__formato = formato
 
     # methods
     def loadSong(self):
-        self.__rawData = open(self.__path, 'rb').read()
+        return open(self.__path, 'rb').read()
     
-    def playSong(self):
-        self.__song = AudioSegment.from_file(io.BytesIO(self.__rawData), format=self.__formato)
-        play(self.__song)
+    def playSong(self, rawData):
+        song = AudioSegment.from_file(io.BytesIO(rawData), format=self.__formato)
+        play(song)
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
 from Player import *
 
-path = 'media/american.mp3'
-formato = 'mp3'
 
-player = Player(path, formato)
-song = player.loadSong()
-player.playSong()
+if __name__=='__main__':
+    path = 'media/american.mp3'
+    formato = 'mp3'
+
+    player = Player(path, formato)
+
+    # play song
+    data = player.loadSong()
+    player.playSong(data)
